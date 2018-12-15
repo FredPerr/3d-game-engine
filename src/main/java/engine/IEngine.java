64e814d94a89355f760f20b7a789f6979de89567
Frederic <frederic.perr@gmail.com>
2018-12-15 15:19:10 -0500

    Add basic files.

---
+++ src/main/java/engine/IEngine.java
@@ -0,0 +1,9 @@
+package engine;
+
+/**
+ * Created by KitK4t on 2018-12-15.
+ *
+ * This is a LWJGL version 3.2 developed engine.
+ */
+public interface IEngine {
+}
---
+++ src/main/java/test/TestEngine.java
@@ -0,0 +1,12 @@
+package test;
+
+/**
+ * Created by KitK4t on 2018-12-15.
+ */
+public class TestEngine {
+
+    /**test.TestEngine method to test the engine.*/
+    public static void main(String[] args){
+        System.out.println("Testing the engine.");
+    }
+}
