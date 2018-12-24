599ac155c2947414bdae7bec5cf29d60ae9ab38f
KitK4t <frederic.norrep@gmail.com>
2018-12-23 22:58:14 -0500

    Adding Location and MathUtil class.

---
+++ src/main/java/engine/util/Location.java
@@ -0,0 +1,91 @@
+package engine.util;
+
+/**The location class is a 3D position (x , y and z).*/
+public class Location {
+
+    public float x,y,z;
+
+    /**Default constructor of the location class.
+     * Creates a location with 0 values.*/
+    public Location(){
+        set(0,0,0);
+    }
+
+    /**Main constructor of the location class.
+     * @param x Value of the x coordinate.
+     * @param y Value of the x coordinate.
+     * @param z Value of the x coordinate.*/
+    public Location(float x, float y, float z){
+        set(x,y,z);
+    }
+
+    /**Set the values of the location.
+     * @param x Value of the x coordinate.
+     * @param y Value of the x coordinate.
+     * @param z Value of the x coordinate.*/
+    public void set(float x, float y, float z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    /**Set the x value of the location.
+     * @param x Value of the x coordinate.*/
+    public void setX(float x){
+        this.x = x;
+    }
+
+    /**Set the y value of the location.
+     * @param y Value of the x coordinate.*/
+    public void setY(float y){
+        this.y = y;
+    }
+
+    /**Set the z value of the location.
+     * @param z Value of the x coordinate.*/
+    public void setZ(float z){
+        this.z = z;
+    }
+
+    /**Add the x value of the location.
+     * @param x Value of the x coordinate to add.*/
+    public void addX(float x){
+        this.x += x;
+    }
+
+    /**Add the y value of the location.
+     * @param y Value of the x coordinate to add.*/
+    public void addY(float y){
+        this.y += y;
+    }
+
+    /**Add the z value of the location.
+     * @param z Value of the x coordinate to add.*/
+    public void addZ(float z){
+        this.z += z;
+    }
+
+    /**@return X value of the location.*/
+    public float getX(){
+        return this.x;
+    }
+
+    /**@return Y value of the location.*/
+    public float getY(){
+        return this.y;
+    }
+
+    /**@return Z value of the location.*/
+    public float getZ(){
+        return this.y;
+    }
+
+    /**Get the distance between 2 3D locations.
+     * @param location Location to get the distance from.
+     * @return Distance between the two locations.*/
+    public double distance(Location location){
+        return Math.sqrt((location.getX()-getX())*(location.getX()-getX())+
+                (location.getY()-getY())*(location.getY()-getY())+
+                (location.getZ()-getZ())*(location.getZ()-getZ()));
+    }
+}
---
+++ src/main/java/engine/util/MathUtil.java
@@ -0,0 +1,9 @@
+package engine.util;
+
+/**The math util class provides functions to do math operations.*/
+public class MathUtil {
+
+    //TODO random location between to 3D points.
+    //TODO distance between to 3D points.
+    //TODO random value (int, float, boolean) with max and min
+}
