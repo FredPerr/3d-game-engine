4672db8c85aa90a5004313e93cf10e52bdb1bf1a
KitK4t <frederic.norrep@gmail.com>
2018-12-24 17:19:22 -0500

    Adding random functions

---
+++ src/main/java/engine/render/Renderer.java
@@ -0,0 +1,8 @@
+package engine.render;
+
+/**The render class is an abstract object that render things to the screen.*/
+public abstract class Renderer {
+
+    //TODO complete
+    public abstract void render();
+}
