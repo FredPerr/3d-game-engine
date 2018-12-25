8033ee14476d85c750901b071df6ec5b61496236
KitK4t <frederic.norrep@gmail.com>
2018-12-25 14:59:18 -0500

    Deleted the Renderer abstract class.

--- src/main/java/engine/render/Renderer.java
+++
@@ -1,8 +0,0 @@
-package engine.render;
-
-/**The render class is an abstract object that render things to the screen.*/
-public abstract class Renderer {
-
-    //TODO complete
-    public abstract void render();
-}
