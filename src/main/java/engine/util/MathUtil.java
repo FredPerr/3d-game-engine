b7c3558c63089f31d24f43b81b50bedc01c10821
KitK4t <frederic.norrep@gmail.com>
2018-12-27 00:38:17 -0500

    Make class static.

--- src/main/java/engine/util/MathUtil.java
+++ src/main/java/engine/util/MathUtil.java
@@ -8,19 +8,19 @@ public class MathUtil {
     private static Random random = new Random();
 
     /**@return Random boolean.*/
-    public boolean randomBoolean(){
+    public static boolean randomBoolean(){
         return random.nextBoolean();
     }
 
     /**@return Random float between 0 and 1.*/
-    public float randomFloat(){
+    public static float randomFloat(){
         return random.nextFloat();
     }
 
     /**@return Random float between 0 and 1.
      * @param max Max inclusive.
      * @param min Min inclusive.*/
-    public float randomInt(int min, int max){
+    public static float randomInt(int min, int max){
         int v = random.nextInt();
         while(v < min || v > max)
             v = random.nextInt();
